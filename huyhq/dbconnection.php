3846303f83b707cd3ea62aee46a4043ce5345e98
huy96cpqnvn <huy96cpqnvn@gmail.com>
2020-04-18 16:00:29 +0700

    Create dbconnection.php

---
+++ huyhq/dbconnection.php
@@ -0,0 +1,124 @@
+<?php
+function login($u, $p) {
+	$hn = "localhost";
+	$db = "t1904e_employees";
+	$username = "root";
+	$password = "";
+	
+		$conn = new mysqli($hn, $username, $password, $db);
+		
+		if ($conn->connect_error) {
+			//die($conn->connect_error);
+			return false;
+		}
+		
+		$select = "select * from users where username = ? and password = ? ";
+		$stmt = $conn->prepare($select);
+		$x = md5($p);
+		$stmt->bind_param("ss", $u, $x);
+		$stmt->execute();
+
+	$result = $stmt->get_result();
+	
+	$num_row = $result->num_rows;
+	
+	if($num_row == 1) {
+		return true;
+	}
+	return false;
+	
+	}
+        
+        function getAllEmployees() {
+            $hn = 'localhost';
+            $db = 't1904e_employees';
+            $username = 'root';
+            $password = '';
+            
+            $conn = new mysqli($hn, $username, $password, $db);
+            
+            if($conn->connect_error) {
+                return false;
+            }
+            
+            $select = "select * from employees ";
+            $result = $conn->query($select);
+            
+            $employees = [];
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $employees[] = $row;
+            }
+            return $employees;
+        }
+        function addEmployee($name, $email, $address, $phone) {
+            $hn = 'localhost';
+            $db = 't1904e_employees';
+            $username = 'root';
+            $password = '';
+            
+            $conn = new mysqli($hn, $username, $password, $db);
+            
+            if($conn->connect_error) {
+                return false;
+            }
+            
+            $insert = "insert into employees(name, email, address, phone) values (?, ?, ?, ?)";
+            $stmt = $conn->prepare($insert);
+            $stmt->bind_param("ssss", $name, $email, $address, $phone);
+            $stmt->execute();
+        }
+        function deleteEmployee($id) {
+            $hn = 'localhost';
+            $db = 't1904e_employees';
+            $username = 'root';
+            $password = '';
+            
+            $conn = new mysqli($hn, $username, $password, $db);
+            
+            if($conn->connect_error) {
+                return false;
+            }
+            $delete = "delete from employees where id = ?";
+            $stmt = $conn->prepare($delete);
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+        }
+        
+        function getEmployeeById($id) {
+            $hn = 'localhost';
+            $db = 't1904e_employees';
+            $username = 'root';
+            $password = '';
+            
+            $conn = new mysqli($hn, $username, $password, $db);
+            
+            if($conn->connect_error) {
+                return false;
+            }
+            $select = "select * from employees where id = ?";
+            $stmt = $conn->prepare($select);
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            
+            $result = $stmt->get_result();
+            
+            $employee = $result->fetch_assoc();
+        }
+        function updateEmployee ($name, $email, $address, $phone, $id) {
+            $hn = 'localhost';
+            $db = 't1904e_employees';
+            $username = 'root';
+            $password = '';
+            
+            $conn = new mysqli($hn, $username, $password, $db);
+            
+            if($conn->connect_error) {
+                return false;
+            }
+            $update = "update employees set name = ?, email = ?, address = ?, phone = ? where id = ?";
+            $stmt = $conn->prepare($update);
+            $stmt->bind_param("ssssi", $name, $email, $address, $phone, $id);
+            $stmt->execute();
+        }
+	?>
+
