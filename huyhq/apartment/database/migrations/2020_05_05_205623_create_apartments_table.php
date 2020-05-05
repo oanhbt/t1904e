0e13d4c37b16128b8720ff083ac0bbeba0198c4d
huy96cpqnvn <huy96cpqnvn@gmail.com>
2020-05-06 03:11:51 +0700

    exam

---
+++ huyhq/apartment/database/migrations/2020_05_05_205623_create_apartments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            
+            $table->String('name');
+            $table->String('address');
+            $table->double('price');
+            $table->String('summary');
+            $table->text('detail');
+
+            $table->String('image');
+            $table->String('status');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
