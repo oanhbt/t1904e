f5c311f20fb0f6cbc31059183b3b233019e54a57
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-05-06 00:11:59 +0700

    Tuấn - ASM

--- Nguyen_Van_Tuan/apartment/resources/views/Apartment/list.blade.php
+++ Nguyen_Van_Tuan/apartment/resources/views/Apartment/list.blade.php
@@ -6,14 +6,16 @@
     <div class="row">
       @foreach ($lsApart as $apart)
       <div class="col-md-4 p-3">
-        <div class="border p-3" style="min-height: 420px; background-color:#efefef;">
+        <div class="border p-3" style="min-height: 570px; background-color:#efefef;">
+          <p>
+            <img src="images/nha.jpg" alt="" style="max-width:100%;">
+          </p>
           <h2>{{$apart->title}}</h2>
-          <p>{{$apart->address}}</p>
-          <p>{{$apart->price}}</p>
-          <p>{{$apart->summary}}</p>
-          <p>{{$apart->content}}</p>
-          <p>{{$apart->image}}</p>
-          <p>{{$apart->status}}</p>
+          <p><strong>Địa chỉ:</strong> {{$apart->address}}</p>
+          <p><strong>Giá bán:</strong> {{$apart->price}}</p>
+          <p><strong><i>Sơ lược:</i></strong> {{$apart->summary}}</p>
+          <p><strong><i>Chi tiết:</i></strong> {{$apart->content}}</p>
+          <p><strong><i>Trạng thái:</i></strong> {{$apart->status}}</p>
         </div>
       </div>
       @endforeach
