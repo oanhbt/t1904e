8f596d7d6d964482fe2441c533eea44968d00065
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-05-13 10:32:09 +0700

    Tuấn

---
+++ Nguyen_Van_Tuan/blog/resources/views/single.blade.php
@@ -0,0 +1,124 @@
+@extends('layouts.frontend')
+
+@section('content')
+<div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('images/img_2.jpg');">
+  <div class="container">
+    <div class="row same-height justify-content-center">
+      <div class="col-md-12 col-lg-10">
+        <div class="post-entry text-center">
+          <span class="post-category text-white bg-success mb-3">{{$post->category->name}}</span>
+          <h1 class="mb-4"><a href="#">{{$post->title}}</a></h1>
+          <div class="post-meta align-items-center text-center">
+            <figure class="author-figure mb-0 mr-3 d-inline-block"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+            <span class="d-inline-block mt-1">By {{$post->user->name}}</span>
+            <span>&nbsp;-&nbsp; {{$post->created_at}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<section class="site-section py-lg">
+  <div class="container">
+    <div class="row blog-entries element-anumate">
+      <div class="col-md-12 col-lg-8 main-content">
+        {!!$post->content!!}
+        <div class="pt-5">
+          <h3 class="mb-5">{{$post->comment()->count()}} Comment</h3>
+        </div>
+        <ul>
+          @foreach($post->comment() as $commt)
+            <li class="Comment">
+              <div class="comment-body">
+                <h3>{{$comm->name}}</h3>
+                <div class="meta">
+                  {{$comm->content}}
+                </div>
+              </div>
+            </li>
+          @endforeach
+        </ul>
+
+        <div class="comment-form-wrap pt-5">
+                  <h3 class="mb-5">Leave a comment</h3>
+                  <form action="{{asset('post_comment')}}" class="p-5 bg-light" method="POST">
+                    @csrf
+                    <input type="hidden" name="post_id" value="{{$post->id}}">
+                    <div class="form-group">
+                      <label for="name">Name *</label>
+                      <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="form-group">
+                      <label for="email">Email *</label>
+                      <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="form-group">
+                      <label for="message">Message</label>
+                      <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                      <input type="submit" value="Post Comment" class="btn btn-primary">
+                    </div>
+
+                  </form>
+                </div>
+      </div>
+
+      <div class="col-md-12 col-lg-4 sidebar">
+        <div class="sidebar-box">
+                      <h3 class="heading">Popular Posts</h3>
+                      <div class="post-entry-sidebar">
+                        <ul>
+                          <li>
+                            <a href="">
+                              <img src="images/img_1.jpg" alt="Image placeholder" class="mr-4">
+                              <div class="text">
+                                <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                                <div class="post-meta">
+                                  <span class="mr-2">March 15, 2018 </span>
+                                </div>
+                              </div>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="">
+                              <img src="images/img_2.jpg" alt="Image placeholder" class="mr-4">
+                              <div class="text">
+                                <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                                <div class="post-meta">
+                                  <span class="mr-2">March 15, 2018 </span>
+                                </div>
+                              </div>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="">
+                              <img src="images/img_3.jpg" alt="Image placeholder" class="mr-4">
+                              <div class="text">
+                                <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                                <div class="post-meta">
+                                  <span class="mr-2">March 15, 2018 </span>
+                                </div>
+                              </div>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <!-- END sidebar-box -->
+
+                    <div class="sidebar-box">
+                      <h3 class="heading">Categories</h3>
+                      <ul class="categories">
+                        @foreach($allCategory as $cate)
+                        <li><a href="#">{{$cate->name}} <span>({{$cate->posts()->count()}})</span></a></li>
+                        @endforeach
+                      </ul>
+                    </div>
+                    <!-- END sidebar-box -->
+      </div>
+
+    </div>
+  </div>
+</section>
+@endsection
--- Nguyen_Van_Tuan/blog/resources/views/welcome.blade.php
+++ Nguyen_Van_Tuan/blog/resources/views/welcome.blade.php
@@ -1,58 +1,57 @@
 @extends('layouts.frontend')
 @section('content')
+
+
 <div class="site-section bg-light">
   <div class="container">
     <div class="row align-items-stretch retro-layout-2">
       <div class="col-md-4">
-        <a href="single.html" class="h-entry mb-30 v-height gradient" style="background-image: url('{{$lsLastestPost[0]->cover}}');">
+        <a href="single.html/{{$lsLastestPost[0]->id}}" class="h-entry mb-30 v-height gradient" style="background-image: url('{{$lsLastestPost[0]->cover}}');">
 
           <div class="text">
-            <h2>{{$lsLastestPost[0]->title}}</h2>
-            <span class="date">{{date('d/M/Y', strtotime($lsLastestPost[0]->created_at))}}</span>
+            <h2>{{$lsLastestPost[0]->tile}}</h2>
+            <span class="date">{{$lsLastestPost[0]->created_at}}</span>
           </div>
         </a>
-        <a href="single.html" class="h-entry v-height gradient" style="background-image: url('{{$lsLastestPost[1]->cover}}');">
+        <a href="single.html/{{$lsLastestPost[1]->id}}" class="h-entry v-height gradient" style="background-image: url('{{$lsLastestPost[1]->cover}}');">
 
           <div class="text">
             <h2>{{$lsLastestPost[1]->title}}</h2>
-            <span class="date">{{date('d/M/Y', strtotime($lsLastestPost[1]->created_at))}}</span>
+            <span class="date">{{$lsLastestPost[1]->created_at}}</span>
           </div>
         </a>
       </div>
       <div class="col-md-4">
-        <a href="single.html" class="h-entry img-5 h-100 gradient" style="background-image: url('{{$lsLastestPost[2]->cover}}');">
+        <a href="single.html/{{$lsLastestPost[2]->id}}" class="h-entry img-5 h-100 gradient" style="background-image: url('{{$lsLastestPost[2]->cover}}');">
 
           <div class="text">
             <div class="post-categories mb-3">
-              <span class="post-category bg-danger">Travel</span>
-              <span class="post-category bg-primary">Food</span>
+              <span class="post-category bg-danger">{{$lsLastestPost[2]->category->name}}</span>
             </div>
-            <h2>{{$lsLastestPost[2]->title}}</h2>
-            <span class="date">{{date('d/M/Y', strtotime($lsLastestPost[2]->created_at))}}</span>
+            <h2>{{isset($lsLastestPost[2]) ? $lsLastestPost[2]->title : $lsLastestPost[0]->title}}</h2>
+            <span class="date">{{$lsLastestPost[2]->created_at}}</span>
           </div>
         </a>
       </div>
       <div class="col-md-4">
-        <a href="single.html" class="h-entry mb-30 v-height gradient" style="background-image: url('{{$lsLastestPost[3]->cover}}');">
+        <a href="single.html/{{$lsLastestPost[3]->id}}" class="h-entry mb-30 v-height gradient" style="background-image: url('{{$lsLastestPost[3]->cover}}');">
 
           <div class="text">
             <h2>{{$lsLastestPost[3]->title}}</h2>
-            <span class="date">{{date('d/M/Y', strtotime($lsLastestPost[3]->created_at))}}</span>
+            <span class="date">{{$lsLastestPost[3]->created_at}}</span>
           </div>
         </a>
-        <a href="single.html" class="h-entry v-height gradient" style="background-image: url('{{$lsLastestPost[4]->cover}}');">
+        <a href="single.html/{{$lsLastestPost[4]->id}}" class="h-entry v-height gradient" style="background-image: url('{{$lsLastestPost[4]->cover}}');">
 
           <div class="text">
             <h2>{{$lsLastestPost[4]->title}}</h2>
-            <span class="date">{{date('d/M/Y', strtotime($lsLastestPost[4]->created_at))}}</span>
+            <span class="date">{{$lsLastestPost[4]->created_at}}</span>
           </div>
         </a>
       </div>
     </div>
   </div>
 </div>
-
-
 
 <div class="site-section">
   <div class="container">
@@ -66,7 +65,7 @@
 @foreach($lsPost as $post)
       <div class="col-lg-4 mb-4">
         <div class="entry2">
-          <a href="single.html"><img src="{{asset('/'.$post->cover)}}" alt="" title=""></a>
+          <a href="single.html/{{$post->id}}"><img src="{{asset('/'.$post->cover)}}" alt="" title=""></a>
           <div class="excerpt">
           <span class="post-category text-white bg-danger mb-3">{{$post->category->name}}</span>
 
@@ -100,10 +99,10 @@
 @foreach($lsCategory as $category)
     <div class="row align-items-stretch retro-layout">
       <div class="col-md-6 order-md-2">
-        <a href="single.html" class="hentry img-2 v-height mb30 gradient" style="background-image: url('images/img_4.jpg');">
+        <a href="single.html/{{$category->posts()->orderBy('created_at', 'DESC')->first()->id}}" class="hentry img-2 v-height mb30 gradient" style="background-image: url('images/img_4.jpg');">
           <span class="post-category text-white bg-danger">{{$category->name}}</span>
           <div class="text">
-            <h2>{{$category->posts()->orderBy('created_at', 'DESC')->first()}}</h2>
+            <h2>{{$category->posts()->orderBy('created_at', 'DESC')->first()->title}}</h2>
             <span>February 12, 2019</span>
           </div>
         </a>
